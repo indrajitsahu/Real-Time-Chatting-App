9eb1b95e7ca6d1338abb8e45528149416a34dcdf
Indrajit Sahu <indrajit93388@gmail.com>
2022-07-16 19:28:00 +0530

    realtime-chat-app v2.0.0

---
+++ public/client.js
@@ -0,0 +1,53 @@
+const socket = io();
+let name;
+let text = document.querySelector("#textarea");
+let messageArea = document.querySelector(".message__area");
+var audio = new Audio('ting1.mp3');
+
+do {
+  name = prompt("Please enter your name:");
+} while (!name);
+
+text.addEventListener("keyup", (e) => {
+  if (e.key === "Enter") {
+    sendMessage(e.target.value);
+    text.value=null;
+  }
+});
+
+function sendMessage(masg) {
+  let msgUser = {
+    user: name,
+    message: masg.trim(),
+  };
+  // message append
+  appendMessage(msgUser, "outgoing");
+
+  // Send to server
+  socket.emit('message', msgUser)
+}
+
+function appendMessage(msg, type) {
+  let mainDiv = document.createElement('div');
+  let className = type;
+  mainDiv.classList.add(className, "message");
+  let markup = 
+        `   <h4>${msg.user}</h4>
+            <p>${msg.message}</p>
+        `;
+  mainDiv.innerHTML = markup;
+  messageArea.appendChild(mainDiv);
+  scrollToBottom();
+}
+
+// Recieve message
+socket.on('message', (rmsg)=>{
+    // console.log(rmsg);
+    appendMessage(rmsg, 'incoming')
+    scrollToBottom();
+    audio.play();
+})
+
+function scrollToBottom(){
+    messageArea.scrollTop = messageArea.scrollHeight;
+}
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const PORT = process.env.PORT | 3000;
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/index.html') 
+    // res.send('Hello') 
+})  
+
+app.use(express.static(__dirname + '/public'))
+
+http.listen(PORT, ()=>{
+    console.log(`Listening on port ${PORT}`);
+})
+
+// Socket code
+
+const io = require('socket.io')(http);
+
+io.on('connection', (socket) => {
+    // console.log('Connected...')
+    socket.on('message', (msg) => {
+        socket.broadcast.emit('message', msg)
+    })
+})
