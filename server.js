1a17c5d81b5f32b2f0c06443aaba36d5fc9d78a3
Indrajit Sahu <indrajit93388@gmail.com>
2022-07-20 10:55:48 +0530

    realtime-chat-app v.2.0.2

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
-const PORT = process.env.PORT | 3000;
+const PORT = process.env.PORT || 3000;
  
 app.get('/', (req, res)=>{
     res.sendFile(__dirname + '/index.html') 
